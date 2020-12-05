fb935ec712ac6d9cec1e1391cbe39d779ddc874b
Grayson White <graysonwhite13@gmail.com>
2020-12-05 11:47:37 -0800

    fix hb_area

--- modeling-functions/hb_area.R
+++ modeling-functions/hb_area.R
@@ -7,23 +7,25 @@ hb_area <- function(data, formula, small_area) {
   model_frame <- model.frame(formula, data) %>%
     dplyr::mutate(small_area = data[[small_area]])
   colnames(model_frame) <- c("y", "x", "small_area")
-  ## Aggregate to area-level
-  model_frame <- model_frame %>%
+  # Direct X
+  X <- model_frame %>%
     group_by(small_area) %>%
     dplyr::summarize(
-      mean_y = mean(y),
-      var_y = var(y),
       mean_x = mean(x)
-    )
-  head(model_frame)
+    ) %>%
+    dplyr::arrange(small_area)
+
+  # Compute direct estimate
+  dir <- direct_estimate(model_frame, "y", "small_area") %>%
+    dplyr::mutate(var = SD^2)
   
   # Fit the model
-  mod <- fSAE.Area(est.init = model_frame$mean_y,
-            var.init = model_frame$var_y,
-            X = model_frame %>% dplyr::select(mean_x))
+  mod <- fSAE.Area(est.init = dir$Direct,
+            var.init = dir$var,
+            X = X %>% dplyr::select(mean_x))
   
   # Calculate CoV
-  CoV <- hbsae::SE(mod) / model_frame$mean_y
+  CoV <- hbsae::SE(mod) / dir$Direct
   ## Add to model object
   mod$CoV <- CoV
   
@@ -33,6 +35,6 @@ hb_area <- function(data, formula, small_area) {
 
 # Example model specification:
 
-# mod_area <- hb_area(data = m333,
-#                     formula = BIOLIVE_TPA ~ nlcd11,
-#                     small_area = "subsection")
+mod_area <- hb_area(data = m333,
+                    formula = BIOLIVE_TPA ~ nlcd11,
+                    small_area = "subsection")
