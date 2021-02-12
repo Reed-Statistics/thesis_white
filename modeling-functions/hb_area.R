30c3a51397c467bc128045cb92c83c93a2135400
Grayson White <graysonwhite13@gmail.com>
2021-02-12 13:19:26 -0800

    use hbsae built in function to calculate cov/relse

--- modeling-functions/hb_area.R
+++ modeling-functions/hb_area.R
@@ -31,29 +31,30 @@ hb_area <- function(data, formula, small_area, pop_data) {
   # These do not change the estimates... why?
   # Create scale and shape hyperparameters
   ## these are chosen from Ver Planck et al 
-  # alpha <- 2
-  # beta <- model_frame %>%
-  #   group_by(small_area) %>%
-  #   summarize(
-  #     var = var(y)
-  #   ) %>%
-  #   summarize(scale = sum(var) / nrow(.)) %>%
-  #   pull()
-  # 
-  # df <- 2 * alpha
-  # scale <- beta / alpha
+  alpha <- 2
+  beta <- model_frame %>%
+    group_by(small_area) %>%
+    summarize(
+      var = var(y)
+    ) %>%
+    summarize(scale = sum(var) / nrow(.)) %>%
+    pull()
+
+  df <- 2 * alpha
+  scale <- beta / alpha
 
   # Fit the model
   mod <- fSAE.Area(
     est.init = dir$Direct,
     var.init = dir$var,
-    X = X %>% dplyr::select(mean_x)
-    # nu0 = df,
-    # s20 = scale
+    X = X %>% dplyr::select(mean_x),
+    nu0 = df,
+    s20 = scale
   )
 
   # Calculate CoV
-  CoV <- hbsae::SE(mod) / dir$Direct
+  # CoV <- hbsae::SE(mod) / dir$Direct
+  CoV <- hbsae::relSE(mod)
   ## Add to model object
   mod$CoV <- CoV
 
